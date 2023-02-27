78e42bb5692e876d2ea1fc928465acbf4757ceb8
ditinex <asifakramsk@gmail.com>
2023-02-27 07:32:29 +0530

    rsuite installed

--- src/layouts/Home/index.js
+++ src/layouts/Home/index.js
@@ -1,31 +1,11 @@
-import Header from '@/components/Header/Header';
 import React from 'react';
-import Footer from '@/components/Footer/Footer';
-import Image from 'next/image';
-import Hero from './Hero';
-import Services from './Services';
-import Stats from './Stats';
+import { Button } from 'rsuite';
+
 const Home = () => {
   return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-        <div className="bg-body-color">
-          <Image
-            src="/curve-bg.png"
-            alt="curve-shape-for-design"
-            height={200}
-            width={1300}
-            className="min-w-full"
-          />
-        </div>
-
-        <Services />
-        <Stats/>
-      </main>
-      <Footer />
-    </>
+    <main>
+      <Button appearance="primary">Hello World</Button>
+    </main>
   );
 };
 
